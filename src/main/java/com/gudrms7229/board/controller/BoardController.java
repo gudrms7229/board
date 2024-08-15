c1675a6a1d4e7586fdf9696ac09528f0815bc32a
HyeongGeun You <yoo0211@naver.com>
2024-08-15 14:07:53 +0900

    작성 데이터 전송 및 mybatis 세팅

--- src/main/java/com/gudrms7229/board/controller/BoardController.java
+++ src/main/java/com/gudrms7229/board/controller/BoardController.java
@@ -1,9 +1,11 @@
 package com.gudrms7229.board.controller;
 
+import com.gudrms7229.board.dto.BoardDTO;
 import com.gudrms7229.board.service.BoardService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 @RequiredArgsConstructor //필요한 도구(BoardService)를 자동으로 준비해 주는 역할을 함
@@ -13,5 +15,11 @@ public class BoardController {
     @GetMapping("/save") //경로(/)에 대한 GET 요청을 처리
     public String save() {
         return "save"; // save.html로 이동
+    }
+
+    @PostMapping("/save")
+    public String save(BoardDTO boardDTO) {
+        System.out.println("boardDTO = " + boardDTO);
+        return "index";
     }
 }
---
+++ src/test/java/com/gudrms7229/board/BoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gudrms7229.board;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
